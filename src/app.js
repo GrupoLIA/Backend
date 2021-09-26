20814cf5e89d28e36aa9c94e2e3f36d0633a8e7d
Cristian <borgesvive@hotmail.com>
2021-09-25 23:50:56 -0300

    Agrega el paquete morgan
    morgan is a Node.js and Express middleware to log HTTP requests and errors

--- src/app.js
+++ src/app.js
@@ -2,20 +2,23 @@ import './db/mongoose';
 
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import reviewRoute from './routes/reviewRoute';
 import userRoute from './routes/userRoute';
 import contractRoute from './routes/contractRoute';
-import adminRoute from './routes/adminRoute'
+import adminRoute from './routes/adminRoute';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 app.use(cors());
+app.options('*', cors());
+
 app.use('/api/reviews', reviewRoute);
 app.use('/api/users', userRoute);
 app.use('/api/contracts', contractRoute);
 app.use('/api/admin', adminRoute);
-
 
 export default app;
